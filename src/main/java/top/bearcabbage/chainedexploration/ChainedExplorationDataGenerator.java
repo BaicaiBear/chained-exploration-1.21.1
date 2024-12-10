4d551ae422e5a6d0cbef6ac7ab1a839131fc86f7
Bai Yuxuan <xiaoxiongbaicai@outlook.com>
2024-12-10 14:48:10 +0800

    Initial commit

---
+++ src/main/java/top/bearcabbage/chainedexploration/ChainedExplorationDataGenerator.java
@@ -0,0 +1,11 @@
+package top.bearcabbage.chainedexploration;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChainedExplorationDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
