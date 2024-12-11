d406a63d1ecaefbe6496a0c7ac216331c7e5b61a
hhhor <303347482@qq.com>
2024-12-11 17:37:43 +0800

    test

--- src/main/java/top/bearcabbage/chainedexploration/ChainedExploration.java
+++ src/main/java/top/bearcabbage/chainedexploration/ChainedExploration.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import top.bearcabbage.chainedexploration.command.ClanCommands;
 import top.bearcabbage.chainedexploration.utils.CEConfig;
 
 public class ChainedExploration implements ModInitializer {
@@ -19,5 +20,10 @@ public class ChainedExploration implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		CEConfig config = new CEConfig(FabricLoader.getInstance().getConfigDir().resolve("chained-exploration.json"));
+		config.set("test", "QAQ");
+		config.save();
+		LOGGER.info(config.getString("test"));
+		LOGGER.info("Hello Fabric world!");
 	}
 }
